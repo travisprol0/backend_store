2d92e08b77301a608d22798f7d421e076322f69e
travisprol0 <travisjohnprol@gmail.com>
2020-09-14 16:29:44 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+      get "/users", to: "users#index"
+    end
+  end
+end
