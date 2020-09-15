808a8c838b572d5153691ba162797da9abd07a7a
travisprol0 <travisjohnprol@gmail.com>
2020-09-15 19:35:48 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ travis = User.create(username: "travisprol0", bio: "this is my bio", avatar: "ht
 christiana = User.create(username: "christianamarie", bio: "this is her bio", avatar: "https://avatarfiles.alphacoders.com/547/54795.jpg")
 
 Product.create(title: "light", brand: "etc", model: "idk", sku: 456876, description: "this is the description", price: 1200, quantity: 7, category: "lighting", image: "https://www.stagelightingstore.com/10278-large_default/etc-source-four-ellipsoidal-26-degree-black-7060a1008.jpg")
- Product.create(title: "microphone", brand: "sure", model: "idk", sku: 456876, description: "this is the description", price: 50, quantity: 7, category: "sound", image: "https://media.sweetwater.com/api/i/f-webp__q-82__ha-fe68bdaa76b0d496__hmac-c368c11dab0253723571a20521eaba5a30557cc0/images/items/750/SM58-large.jpg.auto.webp")
+Product.create(title: "light", brand: "etc", model: "idk", sku: 456876, description: "this is the description", price: 1200, quantity: 7, category: "lighting", image: "https://www.stagelightingstore.com/10278-large_default/etc-source-four-ellipsoidal-26-degree-black-7060a1008.jpg")
+Product.create(title: "light", brand: "etc", model: "idk", sku: 456876, description: "this is the description", price: 1200, quantity: 7, category: "lighting", image: "https://www.stagelightingstore.com/10278-large_default/etc-source-four-ellipsoidal-26-degree-black-7060a1008.jpg")
+#  Product.create(title: "microphone", brand: "sure", model: "idk", sku: 456876, description: "this is the description", price: 50, quantity: 7, category: "sound", image: "https://media.sweetwater.com/api/i/f-webp__q-82__ha-fe68bdaa76b0d496__hmac-c368c11dab0253723571a20521eaba5a30557cc0/images/items/750/SM58-large.jpg.auto.webp")
 
 
